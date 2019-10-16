e2ff6dc39255c4b3f6f4598a6fe075c1e4c1f21b
Jp Ganis <james.ganis@gmail.com>
2019-10-16 21:31:48 +0100

    file sep and wip

---
+++ cog.py
@@ -0,0 +1,80 @@
+import string
+import random
+
+mining_stats = {}
+mining_stats["salt"] = 10
+mining_stats["cybrous"] = 4
+
+def mine(v):
+	v = mining_stats[v]
+	return random.randint(0,v)
+
+def generate_name():
+	vowels = ["a","e","i","o","u","y"]
+	consonants = [l for l in string.ascii_lowercase if l not in vowels]
+	
+	name = ""
+	
+	v = random.randint(0,1)
+	for i in range(6):
+		v = (v + 1) % 2
+		
+		if v == 0:
+			name += random.choice(vowels)
+		else:
+			name += random.choice(consonants)
+			
+	return name
+
+class Cog():
+	def __init__(self):
+		self.name = generate_name()
+		self.i = {}
+		self.i["salt"] = 10
+		self.i["cybrous"] = 10
+		self.i["dollars"] = 100
+		self.i["faith"] = 10
+		self.dead = False
+	
+	def mine(self, comm):
+		m = mine(comm)
+		self.i[comm] += m
+		return m
+		
+	def live_day(self):
+		if not self.dead:
+			for comm in ["salt","cybrous","faith"]:
+				if self.i[comm] < 1: self.die()
+				self.i[comm] -= 1
+		
+	def trade(self, cog, terms):
+		if self.dead or cog.dead: return False
+		
+		my_amount = terms[0]
+		my_comm = terms[1]
+		their_amount = terms[2]
+		their_comm = terms[3]
+		
+		valid_deal = self.i[my_comm] >= my_amount and cog.i[their_comm] >= their_amount
+		
+		if valid_deal:
+			self.i[my_comm] -= my_amount
+			self.i[their_comm] += their_amount
+			
+			cog.i[my_comm] += my_amount
+			cog.i[their_comm] -= their_amount
+			
+			print(self, cog, terms)
+			
+		return valid_deal
+	
+	def die(self):
+		self.dead = True
+		print("\n",self.name,"died\n")
+			
+	def __str__(self):
+		if self.dead: return "{}: DEAD".format(self.name)
+		return "{}".format(self.name)
+		
+	def __repr__(self):
+		return self.__str__()
--- cogwatch.py
+++ cogwatch.py
@@ -1,127 +1,81 @@
 import random
-import string
 
-## can mine salt
-## need to eat 1 salt per day to survive
-## can buy/sell salt
-
-def mine(v):
-	return random.randint(0,v)
-
-def generate_name():
-	vowels = ["a","e","i","o","u","y"]
-	consonants = [l for l in string.ascii_lowercase if l not in vowels]
+from cog import Cog
 	
-	name = ""
-	
-	v = random.randint(0,1)
-	for i in range(6):
-		v = (v + 1) % 2
-		
-		if v == 0:
-			name += random.choice(vowels)
-		else:
-			name += random.choice(consonants)
-			
-	return name
+class Populous():
+	Population = []
 	
 	
-
-class Cog():
+class Farmer(Cog):
 	def __init__(self):
-		self.name = generate_name()
-		self.inventory = {}
-		self.inventory["salt"] = 10
-		self.inventory["cybrous"] = 10
-		self.inventory["dollars"] = 10
-		self.inventory["faith"] = 10
-		self.dead = False
-	
-	def mine(self, comm, v):
-		m = mine(v)
-		self.inventory[comm] += m
-		return m
+		Cog.__init__(self)
+		self.name = "farmer "+self.name 
 		
 	def live_day(self):
-		if not self.dead:
-			for comm in ["salt","cybrous","faith"]:
-				if self.inventory[comm] < 1: self.die()
-				self.inventory[comm] -= 1
+		if self.i["salt"] < self.i["cybrous"]:
+			self.mine("salt")
+		else:
+			self.mine("cybrous")
+			
+		Cog.live_day(self)
+		
+class Bouj(Cog):
+	def __init__(self):
+		Cog.__init__(self)
+		self.name = "mr "+self.name 
+		
+	def live_day(self):
+		## buy from richest
+		for comm in ["salt", "cybrous"]:
+			cog_max = max(Populous.Population, key=lambda c: c.i[comm])
+			amount = 2
+			
+			terms = (1 * amount, "dollars", amount, comm)
+			
+			self.trade(cog_max, terms)
+		
+		## sell to poorest
+		for comm in ["salt", "cybrous"]:
+			cog_min = min(Populous.Population, key=lambda c: c.i[comm])
+			amount = 1
+			
+			terms = (amount, comm, 1 * amount, "dollars")
+			self.trade(cog_max, terms)
+			
 	
-	def die(self):
-		self.dead = True
-		print("\n",self.name,"died\n")
-		
-	def trade(self, cog, my_deal, their_deal):
-		if self.dead or cog.dead: return False
-		
-		my_amount = my_deal[0]
-		my_comm = my_deal[1]
-		their_amount = their_deal[0]
-		their_comm = their_deal[1]
-		
-		valid_deal = self.inventory[my_comm] >= my_amount and cog.inventory[their_comm] >= their_amount
-		
-		if valid_deal:
-			self.inventory[my_comm] -= my_amount
-			self.inventory[their_comm] += their_amount
-			
-			cog.inventory[my_comm] += my_amount
-			cog.inventory[their_comm] -= their_amount
-			
-		return valid_deal
-			
-	def __str__(self):
-		if self.dead: return "{}: DEAD".format(self.name)
-		return "{} {}".format(self.name, str(self.inventory))
-		
-	def __repr__(self):
-		return self.__str__()
+		Cog.live_day(self)
 		
 		
 def episode():
-	c = Cog()
-	d = Cog()
-	e = Cog()
-	
-	pop = [c,d,e]
-	
-	d.dollars = 20
+	pop = Populous.Population
+
+	for i in range(2):
+		pop.append(Farmer())
+	for i in range(1):
+		pop.append(Bouj())
+		
 	
 	church = Cog()
 	church.name = "Church"
 	
-	for i in range(300):
-		church.inventory["faith"] = 1000
+	for i in range(30):
+		church.i["faith"] = 1000
 		
 		for cog in pop:
-			for comm in ["salt","cybrous"]:
-				if cog.inventory[comm] < 1:
-					church.trade(cog, (1, comm), (0, "dollars"))
-				church.trade(cog, (1, "faith"), (int(cog.inventory["dollars"] * 0.1), "dollars"))
 			cog.live_day()
+			
+			church.trade(cog, (1, "faith", int(cog.i["dollars"] * 0.1), "dollars"))
+			
+			for comm in ["salt","cybrous"]:
+				if cog.i[comm] <= 1:
+					church.trade(cog, (1, comm, 0, "dollars"))
 		
 		if all([cog.dead for cog in pop]):  break
 	
-		
-		m = c.mine("salt",16)
-		y = e.mine("cybrous",16)
-		
-		m_tax = int(m*0.1)
-		y_tax = int(y*0.1)
-		
-		## church taxes 10% of everything mined in order to help people who are starving
-		church.trade(c, (0, "dollars"), (m_tax, "salt"))
-		church.trade(e, (0, "dollars"), (y_tax, "cybrous"))
-		
-		d.trade(c, (2, "dollars"), (m-m_tax-1, "salt"))
-		d.trade(e, (2, "dollars"), (y-y_tax-1, "cybrous"))
-		
-		d.trade(c, (1, "cybrous"), (3, "dollars"))
-		d.trade(e, (1, "salt"), (3, "dollars"))
-		
-		
-		print(c,"||",d,"||",e)
-	print(church)
+		print()
+		for c in pop:
+			print(c, c.i)
+		print(church, church.i)
+		print()
 		
 episode()
