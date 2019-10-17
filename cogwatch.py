34b98d3fe9560d2e414b2b4e2b9a672624a9083f
Jp Ganis <james.ganis@gmail.com>
2019-10-17 17:27:23 +0100

    alms

--- cogwatch.py
+++ cogwatch.py
@@ -19,8 +19,6 @@ class Farmer(Cog):
 		Cog.live_day(self)
 		
 class Bouj(Cog):
-	numbers = []
-	
 	def __init__(self):
 		Cog.__init__(self)
 		self.name = "mr "+self.name 
@@ -29,8 +27,8 @@ class Bouj(Cog):
 		## buy from richest
 		for comm in ["salt", "cybrous"]:
 			cog_max = max(Populous.Population, key=lambda c: c.i[comm])
-			amount = Bouj.numbers[0]
-			price_per_unit = Bouj.numbers[1]
+			amount = 6
+			price_per_unit = 1
 			
 			terms = (price_per_unit * amount, "dollars", amount, comm)
 			
@@ -39,8 +37,8 @@ class Bouj(Cog):
 		## sell to poorest
 		for comm in ["salt", "cybrous"]:
 			cog_min = min(Populous.Population, key=lambda c: c.i[comm])
-			amount = Bouj.numbers[2]
-			price_per_unit = Bouj.numbers[3]
+			amount = 2
+			price_per_unit = 5
 			
 			terms = (amount, comm, price_per_unit * amount, "dollars")
 			self.trade(cog_max, terms)
@@ -48,60 +46,82 @@ class Bouj(Cog):
 	
 		Cog.live_day(self)
 		
+class Church(Cog):
+	def __init__(self):
+		Cog.__init__(self)
+		self.name = "Church"
+
+	def live_day(self):
+		if self.dead: return
+		
+		self.i["faith"] = 1000
+		
+		for cog in Populous.Population:
+			if cog.i["dollars"] >= 10:
+				self.trade(cog, (4, "faith", int(cog.i["dollars"]*0.1), "dollars"))
+			
+			for comm in ["salt","cybrous"]:
+				if cog.i[comm] <= 9:
+					self.trade(cog, (8, comm, 0, "dollars"))
+		
+		poorest_cog = min([cog for cog in Populous.Population], key=lambda x: x.i["dollars"])
+		self.trade(poorest_cog, (20, "dollars", 0, "dollars"))
+		
+		
+		self.i["dollars"] -= 1
+		
+		if self.i["dollars"] <= 0: self.die()
+	
+		
 		
 def episode(debug=False):
 	Populous.Population = []
 	pop = Populous.Population
 	
-	numbers = [random.randint(0,10) for _ in range(8)]
-	numbers = [6, 1, 2, 5, 4, 0, 9, 8]
-	Bouj.numbers = numbers
-
+	if random.random() >= 0.5:
+		fiu = Farmer()
+		fiu.name = "farmer fiu"
+	else:
+		fiu = Bouj()
+		fiu.name = "mr fiu"
+		
+	pop.append(fiu)
+	
 	for i in range(15):
 		pop.append(Farmer())
 	for i in range(5):
 		pop.append(Bouj())
 		
-	
-	church = Cog()
-	church.name = "Church"
-	
-	for i in range(300):
-		church.i["faith"] = 1000
-		
-		for cog in pop:
-			cog.live_day()
+	church = Church()
+	pop.append(church)
 			
-			church.trade(cog, (numbers[4], "faith", 1+numbers[5], "dollars"))
-			
-			for comm in ["salt","cybrous"]:
-				if cog.i[comm] <= numbers[6]:
-					church.trade(cog, (numbers[7], comm, 0, "dollars"))
-		
-		if all([cog.dead for cog in pop]):  break
-
+	for i in range(300):		
 		if debug:
 			print()
 			for c in pop:
 				print(c, c.i)
-			print(church, church.i)
 			print()
+			
+		for cog in pop:
+			cog.live_day()
 		
-	return len([cog for cog in pop if not cog.dead]), numbers
+		if all([cog.dead for cog in pop]):  break
+
+	return len([cog for cog in pop if not cog.dead])
 		
 		
-# print(episode(True))
+print(episode(True))
 		
-bn = []
-bs = 0
-iters = 1000
-for i in range(iters):
-	print(bs, i/iters, end="\r")
-	s, n = episode()
+# bn = []
+# bs = 0
+# iters = 1000
+# for i in range(iters):
+	# print(bs, i/iters, end="\r")
+	# s, n = episode()
 	
-	if s > bs:
-		bs = s
-		bn = n
+	# if s > bs:
+		# bs = s
+		# bn = n
 		
-# print(episode(True))
-print(bs, bn)
+# # print(episode(True))
+# print(bs, bn)
